c5818a7f2b00dfa0145eb3b301b5e3ca03ef0026
crisine <crisine@knou.ac.kr>
2024-03-07 14:16:55 +0900

    init: initial commit

---
+++ TimeToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToDo
+//
+//  Created by Minho on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
